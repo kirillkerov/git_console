bd0a3d72dedd9a8741459eca00fce713ceb6c61b
Kirill Kerov <kirill.stan1221@yandex.ru>
2022-07-12 09:34:49 +0000

    ready main branch

--- app.php
+++ app.php
@@ -2,6 +2,14 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+use App\GetStringCommand;
+use App\GetUserCommand;
+use App\HelloCommand;
 use Symfony\Component\Console\Application;
 
-asd
+$app = new Application();
+$app->add(new HelloCommand());
+$app->add(new GetStringCommand());
+$app->add(new GetUserCommand());
+
+$app->run();
---
+++ src/HelloCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+
+class HelloCommand extends Command
+{
+    
+}
